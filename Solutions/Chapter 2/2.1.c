5e477e64940951ba49b5dfa328e3e498daa311eb
CFF Windows <60484804+CrazyFFester@users.noreply.github.com>
2025-05-01 15:14:01 +0400

    Finished 2.1.

--- Solutions/Chapter 2/2.1.c
+++ Solutions/Chapter 2/2.1.c
@@ -2,11 +2,35 @@
 #include <limits.h>
 #include <float.h>
 
-int main() {
-    printf("Ranges of diffrent data types:\n\n");
-    printf("Char fromm %d to %d\n", CHAR_MIN, CHAR_MAX);
-    printf("Long int - %d\n", LONG_MAX);
-    printf("Int - %d\n", INT_MAX);
+int main()
+{
+    printf("=============================================\n");
+    printf(" Ranges of unsigned and signed data types\n");
+    printf("=============================================\n\n");
+
+    printf("Signed types:\n");
+    printf("---------------------------------------------\n");
+    printf("Char\t: %d to %d\n", CHAR_MIN, CHAR_MAX);
+    printf("Short\t: %d to %d\n", SHRT_MIN, SHRT_MAX);
+    printf("Int\t: %d to %d\n", INT_MIN, INT_MAX);
+    printf("Long\t: %ld to %ld\n", LONG_MIN, LONG_MAX);
+    printf("\n");
+
+    printf("Unsigned types:\n");
+    printf("---------------------------------------------\n");
+    printf("Char\t: 0 to %u\n", UCHAR_MAX);
+    printf("Short\t: 0 to %u\n", USHRT_MAX);
+    printf("Int\t: 0 to %u\n", UINT_MAX);
+    printf("Long\t: 0 to %lu\n", ULONG_MAX);
+    printf("\n");
+
+    printf("=============================================\n");
+    printf(" Floating-point types range\n");
+    printf("=============================================\n\n");
+
+    printf("Float\t\t: %e to %e\n", FLT_MIN, FLT_MAX);
+    printf("Double\t\t: %e to %e\n", DBL_MIN, DBL_MAX);
+    printf("Long double\t: %e to %e\n", LDBL_MIN, LDBL_MAX);
 
     return 0;
 }
