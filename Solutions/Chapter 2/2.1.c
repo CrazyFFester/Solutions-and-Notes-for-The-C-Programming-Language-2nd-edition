b16cf84453fe69e3312ad39df26f0136963943cc
CFF Windows <60484804+CrazyFFester@users.noreply.github.com>
2025-05-01 14:10:10 +0400

    first version of 2.1.c

---
+++ Solutions/Chapter 2/2.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+    printf("Ranges of diffrent data types:\n\n");
+    printf("Char fromm %d to %d\n", CHAR_MIN, CHAR_MAX);
+    printf("Long int - %d\n", LONG_MAX);
+    printf("Int - %d\n", INT_MAX);
+
+    return 0;
+}
